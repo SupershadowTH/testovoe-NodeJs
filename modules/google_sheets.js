84c83bbbc61f965c4cbaa94be5e09606fd1e23e6
SupershadowTH <60179890+SupershadowTH@users.noreply.github.com>
2024-11-19 16:11:47 +0300

    Add files via upload

--- modules/google_sheets.js
+++ modules/google_sheets.js
@@ -9,28 +9,25 @@ export default async (clients_data) => {
     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
   });
 
-  const doc = new GoogleSpreadsheet(
-    config.sheet_id,
-    serviceAccountAuth
-  );
+  const doc = new GoogleSpreadsheet(config.sheet_id, serviceAccountAuth);
 
-  await doc.loadInfo().then(async ()=>{
-    console.log(`Документ загружен\nИмя документа: ${doc.title}`);
+  await doc
+    .loadInfo()
+    .then(async () => {
+      console.log(`Документ загружен\nИмя документа: ${doc.title}`);
 
-  if (clients_data[0]) {
-    const params = Object.keys(clients_data[0]);
-    const sheet = await doc.addSheet({ headerValues: params });
+      if (clients_data[0]) {
+        const params = Object.keys(clients_data[0]);
+        const sheet = await doc.addSheet({ headerValues: params });
 
-    sheet.addRows(clients_data);
-    console.log(`Ссылка на документ:`);
-    console.log(doc._spreadsheetUrl);
-  } else {
-    return;
-  }
-  }
-  ).catch(()=> {
-    console.log("Ошибка загрузки документа")
-  });
-  
+        sheet.addRows(clients_data);
+        console.log(`Ссылка на документ:`);
+        console.log(doc._spreadsheetUrl);
+      } else {
+        return;
+      }
+    })
+    .catch(() => {
+      console.log("Ошибка загрузки документа");
+    });
 };
-
